fb38b7cbc386ccf22b691a412ada5e1a19e55a20
TahirAtakanCan <atkan4202@gmail.com>
2024-01-03 21:41:23 +0300

    feat: Google Sign In

--- ChatsUp/Resources/AppDelegate.swift
+++ ChatsUp/Resources/AppDelegate.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 import Firebase
-
-
+import GoogleSignIn
+import GoogleSignInSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -20,9 +20,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         FirebaseApp.configure()
         
+        GIDSignIn.sharedInstance.restorePreviousSignIn { user, error in
+            if error != nil || user == nil {
+              // Show the app's signed-out state.
+            } else {
+              // Show the app's signed-in state.
+            }
+          }
+        
+        
         return true
     }
     
+    
+    func application(
+      _ app: UIApplication,
+      open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]
+    ) -> Bool {
+      var handled: Bool
+
+      handled = GIDSignIn.sharedInstance.handle(url)
+      if handled {
+        return true
+      }
+
+      // Handle other custom URL types.
+
+      // If not handled by this app, return false.
+      return false
+    }
+    
+    
+     
 
     // MARK: UISceneSession Lifecycle
 
