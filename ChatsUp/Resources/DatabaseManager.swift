7919f2e86f97056277b41f87670d9114c3a2d603
TahirAtakanCan <atkan4202@gmail.com>
2024-01-03 09:24:46 +0300

    feat: add database and realtime code

--- ChatsUp/Resources/DatabaseManager.swift
+++ ChatsUp/Resources/DatabaseManager.swift
@@ -14,7 +14,7 @@ final class DatabaseManager{
     
     static let shared = DatabaseManager()
     
-    private let database = Database.database().reference()
+    private let database = Database.database(url: "https://chatsupfirebase-79809-default-rtdb.europe-west1.firebasedatabase.app").reference()
     
     
     
@@ -27,7 +27,10 @@ extension DatabaseManager {
     
     public func userExists(with email: String, completion: @escaping (Bool)-> Void) {
         
-        database.child(email).observeSingleEvent(of: .value, with: { snapshot in
+        var safeEmail = email.replacingOccurrences(of: ".", with: "-").replacingOccurrences(of: "@", with: "-")
+        
+        
+        database.child(safeEmail).observeSingleEvent(of: .value, with: { snapshot in
             
             guard snapshot.value as? String != nil else {
                 completion(false)
@@ -41,7 +44,7 @@ extension DatabaseManager {
     
     /// Insert new user to database
     public func insertUser(with user: ChatsUpUser) {
-        database.child(user.emailAddress).setValue([
+        database.child(user.safeEmail).setValue([
             "first_name": user.firstName,
             "last_name": user.lastName
         
@@ -55,6 +58,13 @@ struct ChatsUpUser {
     let firstName: String
     let lastName: String
     let emailAddress: String
+    
+    var safeEmail: String {
+        var safeEmail = emailAddress.replacingOccurrences(of: ".", with: "-")
+        safeEmail = safeEmail.replacingOccurrences(of: "@", with: "-")
+        return safeEmail
+    }
+        
     //let profilePictureUrl: String
     
     
