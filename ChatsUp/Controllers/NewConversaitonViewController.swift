a2361ad04115d3e14f0cfee4bbf39b1228aeb6fd
TahirAtakanCan <atkan4202@gmail.com>
2024-01-07 10:10:35 +0300

    feat: finish Building User Interfaces and Dependecies

--- ChatsUp/Controllers/NewConversaitonViewController.swift
+++ ChatsUp/Controllers/NewConversaitonViewController.swift
@@ -6,16 +6,60 @@
 //
 
 import UIKit
+import JGProgressHUD
 
 class NewConversaitonViewController: UIViewController {
-
+    
+    private let spinner = JGProgressHUD()
+    
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search for Users..."
+        return searchBar
+    }()
+    
+    private let tableView: UITableView = {
+        let table = UITableView()
+        table.isHidden = true
+        table.register(UITableViewCell.self,
+                       forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private let noResultsLabel: UILabel = {
+        let label = UILabel()
+        label.isHidden = true
+        label.text = "No Results"
+        label.textAlignment = .center
+        label.textColor = .gray
+        label.font = .systemFont(ofSize: 21, weight: .medium)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchBar.delegate = self
+        let blueGreenColor = UIColor(red: 0, green: 0.5, blue: 0.5, alpha: 1)
+        view.backgroundColor = blueGreenColor
         
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "cancel",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(dismissSelf))
+        searchBar.becomeFirstResponder()
     }
     
-
+    @objc private func dismissSelf() {
+        dismiss(animated: true, completion: nil)
+    }
     
+}
 
+
+extension NewConversaitonViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        return
+    }
 }
