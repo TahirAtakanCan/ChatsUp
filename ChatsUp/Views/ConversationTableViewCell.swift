2cfb0d7c059e904756b05641767c14581dbd2f41
TahirAtakanCan <atkan4202@gmail.com>
2024-02-29 12:51:23 +0300

    feat: delete CWC and add TableViewController

---
+++ ChatsUp/Views/ConversationTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ConversationTableViewCell.swift
+//  ChatsUp
+//
+//  Created by Tahir Atakan Can on 29.02.2024.
+//
+
+import UIKit
+
+class ConversationTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
